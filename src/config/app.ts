7174b1120eeff6cb9ec69f353cefc8543d8e490c
Samuel <72594810+samuelpeybernes-dev@users.noreply.github.com>
2023-09-09 00:52:30 +0200

    config v2

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import helmet from 'helmet'
-
-//import authMiddleware from '../middlewares/authMiddleware.js'
+import projectNotes from '../routes/projectNotes'
+import authMiddleware from '../middlewares/authMiddleware.js'
 
 
 const app = express()
@@ -16,6 +16,7 @@ app.get('/', (req, res) => res.json('Home de Cron ⏲'))
 app.get('/api', (req, res) => res.json({ data: "Bienvenue sur l'api ⏲" }))
 app.get('/api/v1', (req, res) => res.json("Bienvenue sur la v1 de l'api ⏲"))
 
+app.use('/api/v1/projectNotes', authMiddleware, projectNotes)
 
 app.use('*', (req, res) =>
   res.status(404).json({
