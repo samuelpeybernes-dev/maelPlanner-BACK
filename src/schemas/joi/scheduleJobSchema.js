38e0608789f0470aa343bda803307ab28e6a0132
Samuel <samuelpeybernes33@gmail.com>
2023-09-11 17:52:51 +0200

    ree

--- src/schemas/joi/scheduleJobSchema.js
+++ src/schemas/joi/scheduleJobSchema.js
@@ -1,9 +1,9 @@
 import Joi from 'joi'
 
 const scheduleJobSchema = Joi.object({
-  id: Joi.number().min(0).max(999999999),
-  newStart: Joi.date().iso(),
-  newEnd: Joi.date().iso(),
+  id: Joi.string(),
+  newStart: Joi.string().isoDate(),
+  newEnd: Joi.string().isoDate(),
   newText: Joi.string()
     .regex(/^[a-zA-ZÀ-ÿ0-9\s'-.?!]+$/u)
     .min(1)
