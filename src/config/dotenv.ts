aa754625cee72edc5c68719b483bf807c471f2b3
Samuel <72594810+samuelpeybernes-dev@users.noreply.github.com>
2023-09-20 14:00:20 +0200

    test2

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv-safe'
+/*import * as dotenv from 'dotenv-safe'
 import path from 'path'
 
 dotenv.config()
@@ -14,13 +14,13 @@ switch (process.env.NODE_ENV) {
     dotenvPath = path.join(__dirname, '.env') 
 }
 dotenv.config({ path: dotenvPath })
-
+*/
 const MONGO_DB_HOST: string = process.env.MONGO_DB_HOST!
 const MONGO_DB_USERNAME: string = process.env.MONGO_DB_USERNAME!
 const MONGO_DB_PASSWORD: string = process.env.MONGO_DB_PASSWORD!
 const MONGO_DB_NAME: string = process.env.MONGO_DB_NAME!
 export const mongoUrl = `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOST}/${MONGO_DB_NAME}?authSource=admin&w=1`
-
+/*
 export const NODE_ENV: string = process.env.NODE_ENV!
 export const BAGDAD_PORT: number = NODE_ENV === 'development' ? 1632 : 1631
 // eslint-disable-next-line eqeqeq
@@ -29,3 +29,4 @@ export const ENABLE_CACHE: boolean = process.env.ENABLE_CACHE! !== 'false'
 
 
 export const JWT_PUB_KEY_PATH: string = process.env.JWT_PUB_KEY_PATH!
+*/
