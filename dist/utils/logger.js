140e12fb01fd8072df8e1feb67632c9df645fdb1
Samuel <samuelpeybernes33@gmail.com>
2023-10-07 14:10:50 +0200

    testttt

--- dist/utils/logger.js
+++ dist/utils/logger.js
@@ -7,23 +7,20 @@ const logLevels = {
     warn: 2,
     info: 3,
     debug: 4,
-    trace: 5
+    trace: 5,
 };
 const logger = (0, winston_1.createLogger)({
-    level: "debug",
+    level: 'debug',
     levels: logLevels,
     format: winston_1.format.combine(winston_1.format.colorize(), winston_1.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
+        format: 'YYYY-MM-DD HH:mm:ss',
     }), winston_1.format.errors({ stack: true }), winston_1.format.printf(({ timestamp, level, message }) => {
         if (message instanceof Object) {
             return `[${timestamp}] ${level}: ${JSON.stringify(message, null, 4)}`;
         }
         return `[${timestamp}] ${level}: ${message}`;
     })),
-    transports: [
-        new winston_1.transports.File({ filename: './logs/error.log', level: 'error' }),
-        new winston_1.transports.Console()
-    ],
+    transports: [new winston_1.transports.File({ filename: '../logs/error.log', level: 'error' }), new winston_1.transports.Console()],
 });
 exports.default = logger;
 //# sourceMappingURL=logger.js.map
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,35 +1,31 @@
-import { createLogger, format, transports } from 'winston';
-
+import { createLogger, format, transports } from 'winston'
 
 const logLevels = {
-    fatal : 0,
-    error : 1,
-    warn  : 2,
-    info  : 3,
-    debug : 4,
-    trace : 5
-};
+  fatal: 0,
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  trace: 5,
+}
 
 const logger = createLogger({
-    level: "debug",
-    levels: logLevels,
-    format: format.combine(
-        format.colorize(),
-        format.timestamp({
-          format: 'YYYY-MM-DD HH:mm:ss'
-        }),
-        format.errors({ stack: true }),
-        format.printf(({ timestamp, level, message }) => {
-          if(message instanceof Object){
-            return `[${timestamp}] ${level}: ${JSON.stringify(message, null, 4)}`;
-          }
-            return `[${timestamp}] ${level}: ${message}`;
-        })
-      ),
-      transports: [
-        new transports.File({ filename: './logs/error.log', level: 'error' }),
-        new transports.Console()
-      ],
-});
+  level: 'debug',
+  levels: logLevels,
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.errors({ stack: true }),
+    format.printf(({ timestamp, level, message }) => {
+      if (message instanceof Object) {
+        return `[${timestamp}] ${level}: ${JSON.stringify(message, null, 4)}`
+      }
+      return `[${timestamp}] ${level}: ${message}`
+    })
+  ),
+  transports: [new transports.File({ filename: '../logs/error.log', level: 'error' }), new transports.Console()],
+})
 
-export default logger;
+export default logger
