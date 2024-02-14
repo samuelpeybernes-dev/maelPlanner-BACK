9eb74ccbedad3d5e6701a3e1882366d06734c1d6
Samuel <samuelpeybernes33@gmail.com>
2024-02-14 11:02:48 +0100

    debug

--- dist/utils/email/sendEmail.js
+++ dist/utils/email/sendEmail.js
@@ -6,8 +6,10 @@ const fs = require('fs');
 const path = require('path');
 exports.default = async (email, subject, payload, template) => {
     try {
-        // create reusable transporter object using the default SMTP transport
-        const transporter = nodemailer.createTransport({
+        const source = fs.readFileSync(path.join(__dirname, template), 'utf8');
+        const compiledTemplate = handlebars.compile(source);
+        await nodemailer
+            .createTransport({
             host: 'smtp.gmail.com',
             port: 465,
             secure: true,
@@ -15,47 +17,17 @@ exports.default = async (email, subject, payload, template) => {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS, // naturally, replace both with your real credentials or an application-specific password
             },
-        });
-        await new Promise((resolve, reject) => {
-            // verify connection configuration
-            transporter.verify(function (error, success) {
-                if (error) {
-                    console.log(error);
-                    reject(error);
-                }
-                else {
-                    console.log('Server is ready to take our messages');
-                    resolve(success);
-                }
-            });
-        });
-        const source = fs.readFileSync(path.join(__dirname, template), 'utf8');
-        const compiledTemplate = handlebars.compile(source);
-        const options = {
+        })
+            .sendMail({
             from: process.env.SMTP_USER,
             to: email,
-            subject: subject,
-            html: 'test',
-        };
-        // Send email
-        await new Promise((resolve, reject) => {
-            transporter.sendMail(options).then((info, err) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                }
-                else {
-                    console.log(info);
-                    resolve(info);
-                }
-            });
+            subject,
+            html: compiledTemplate(payload),
         });
-        return res.status(200).json({
-            success: true,
-        });
+        console.log('Email sent to ' + email);
     }
-    catch (error) {
-        return error;
+    catch (e) {
+        console.error(e);
     }
 };
 /*
--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -5,56 +5,27 @@ const path = require('path')
 
 export default async (email, subject, payload, template) => {
   try {
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS, // naturally, replace both with your real credentials or an application-specific password
-      },
-    })
-
-    await new Promise((resolve, reject) => {
-      // verify connection configuration
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error)
-          reject(error)
-        } else {
-          console.log('Server is ready to take our messages')
-          resolve(success)
-        }
-      })
-    })
-
     const source = fs.readFileSync(path.join(__dirname, template), 'utf8')
     const compiledTemplate = handlebars.compile(source)
-    const options = {
-      from: process.env.SMTP_USER,
-      to: email,
-      subject: subject,
-      html: 'test',
-    }
-
-    // Send email
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(options).then((info, err) => {
-        if (err) {
-          console.error(err)
-          reject(err)
-        } else {
-          console.log(info)
-          resolve(info)
-        }
+    await nodemailer
+      .createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS, // naturally, replace both with your real credentials or an application-specific password
+        },
       })
-    })
-    return res.status(200).json({
-      success: true,
-    })
-  } catch (error) {
-    return error
+      .sendMail({
+        from: process.env.SMTP_USER,
+        to: email,
+        subject,
+        html: compiledTemplate(payload),
+      })
+    console.log('Email sent to ' + email)
+  } catch (e) {
+    console.error(e)
   }
 }
 
